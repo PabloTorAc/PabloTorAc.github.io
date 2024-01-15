a58b8bfb04ac872b4f85c0d4472c060204073040
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-01-15 13:06:22 -0600

    add: fix br for title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hola, soy Pablo
-            <br className="hidden lg:inline-block" />Soy estudiante de la carrera de Ing en software y sistemas Computacionales.
+            <br className="lg:inline-block" />Soy estudiante de la carrera de Ing en software y sistemas Computacionales.
           </h1>
           <p className="mb-8 leading-relaxed">
           Con habilidades en lenguajes de programación 
